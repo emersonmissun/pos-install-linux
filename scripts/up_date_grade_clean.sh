c7bf7833ca5bbb25817d1a4110292f8bc8717e0d
emersonmissun <emerson.missun@gmail.com>
2023-01-28 22:12:10 -0300

    feat: 🔨 Atualizar Sistema
    Script para atualização e limpeza do sistema

---
+++ scripts/up_date_grade_clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Atuliazação do sistema
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo apt clean -y
