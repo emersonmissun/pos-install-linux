1ec5382425b150b1475dbd7f99736ef615986d2c
emersonmissun <emerson.missun@gmail.com>
2023-03-09 14:28:37 -0300

    feat: 🚚 Arquivo de Parâmetros
    Criado arquivo de parâmetros de dados para configuração dos scripts

---
+++ scripts/00-parametros_exemplo.sh
@@ -0,0 +1,112 @@
+#! /bin/bash
+
+# Parâmetros de configuração do projeto da Consultoria de Publicidade e Marketing Digital Emersão Digital
+
+## Parâmetros de configuração do git e do GitHub
+
+### Dados de usuário e senha do Git 
+
+# Nome de usuário do Git
+git_user=""
+
+# Senha do Git
+git_password=""
+
+# E-mail do usuário do Git
+git_email=""
+
+# Nome do repositório remoto do GitHub
+git_remote_repository=""
+
+### Dados de usuário e senha do GitHub
+
+# Nome de usuário do GitHub
+github_user=""
+
+# Senha do GitHub
+github_password=""
+
+# E-mail do usuário do GitHub
+github_email=""
+
+# Nome do repositório remoto do GitHub
+github_remote_repository=""
+
+## Parâmetros de configuração do servidor web do EasyEngine
+
+# Nome do site
+site_name=""
+
+# Nome do domínio
+site_domain=""
+
+# Endereço de e-mail do administrador do site
+site_admin_email=""
+
+# Nome do usuário do MySQL
+mysql_user=""
+
+# Senha do usuário do MySQL
+mysql_password=""
+
+# Nome do banco de dados do MySQL
+mysql_database=""
+
+# Prefixo das tabelas do banco de dados do MySQL
+mysql_table_prefix=""
+
+# Endereço de e-mail do administrador do banco de dados do MySQL
+mysql_admin_email=""
+
+# Nome do usuário do FTP
+ftp_user=""
+
+# Senha do usuário do FTP
+ftp_password=""
+
+# Nome do banco de dados do PostgreSQL
+postgresql_database=""
+
+# Nome de usuário do PostgreSQL
+postgresql_user=""
+
+# Senha do usuário do PostgreSQL
+postgresql_password=""
+
+# Prefixo das tabelas do banco de dados do PostgreSQL
+postgresql_table_prefix=""
+
+# Endereço de e-mail do administrador do banco de dados do PostgreSQL
+postgresql_admin_email=""
+
+# Nome do usuário do Redis
+redis_user=""
+
+# Senha do usuário do Redis
+redis_password=""
+
+# Endereço de e-mail do administrador do Redis
+redis_admin_email=""
+
+# Nome do usuário do Memcached
+memcached_user=""
+
+# Senha do usuário do Memcached
+memcached_password=""
+
+# Endereço de e-mail do administrador do Memcached
+memcached_admin_email=""
+
+# Nome do usuário do MariaDB
+mariadb_user=""
+
+# Senha do usuário do MariaDB
+mariadb_password=""
+
+# Nome do banco de dados do MariaDB
+mariadb_database=""
+
+# Prefixo das tabelas do banco de dados do MariaDB
+mariadb_table_prefix=""
+
+# Endereço de e-mail do administrador do banco de dados do
