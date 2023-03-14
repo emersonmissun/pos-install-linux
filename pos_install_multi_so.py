41795ba20d9e35ea9bd309b1566035aed3f7bcf1
emersonmissun <emerson.missun@gmail.com>
2023-03-14 15:06:14 -0300

    Menu principal

---
+++ pos_install_multi_so.py
@@ -0,0 +1,5 @@
+#! /env/bin/ python
+
+# Menu principal de pós instalação de sistemas operacionais Linux e Windows em servidores e estações de trabalho.
+## Definir variáveis de configuração do sistema operacional 
+
