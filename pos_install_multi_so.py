85e62ba5577513b46107db498f36833598aa6789
emersonmissun <emerson.missun@gmail.com>
2023-03-22 21:31:36 -0300

    docs: 💄 Alterar TODOs
    Alteração em algumas sugestões de tarefas

--- pos_install_multi_so.py
+++ pos_install_multi_so.py
@@ -1,5 +1,9 @@
 #! /env/bin/ python
 
-# Menu principal de pós instalação de sistemas operacionais Linux e Windows em servidores e estações de trabalho.
+# TODO: Criar menu principal de pós instalação de sistemas operacionais Linux e Windows em servidores e estações de trabalho.
+
 ## Definir variáveis de configuração do sistema operacional 
+
+
+## TODO: Criar uma Função para identificar o Sistema Operacional
 
